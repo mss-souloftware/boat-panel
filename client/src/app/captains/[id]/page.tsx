342b3e81a21b019baff43397b84bc410278ed783
mss-souloftware <sufyan.souloftware@gmail.com>
2024-06-06 12:23:01 +0500

    api fixes

--- client/src/app/captains/[id]/page.tsx
+++ client/src/app/captains/[id]/page.tsx
@@ -6,7 +6,6 @@ import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import { buildUrl } from '../../../../utils/buildUrl';
 
 interface Boat {
-    captain: any;
     data: string;
     fullName: string;
     phone: string;
@@ -37,7 +36,7 @@ export default function BoatDetails() {
                 });
                 if (res.ok) {
                     const data = await res.json();
-                    setBoat(data);
+                    setBoat(data.data.captain);
                     console.log(data);
                 } else {
                     console.error('Failed to fetch captain:', res.status);
@@ -62,7 +61,7 @@ export default function BoatDetails() {
             <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark mx-auto">
                 <div className="px-4 py-6 md:px-6 xl:px-7.5">
                     <h4 className="text-xl font-semibold text-black dark:text-white">
-                        {boat.data.captain.fullName}
+                        {boat.fullName}
                     </h4>
                 </div>
 
@@ -71,7 +70,7 @@ export default function BoatDetails() {
                         <p className="font-medium">Captain Name:</p>
                     </div>
                     <div className="col-span-3 flex items-center">
-                        <p className="">{boat.data.captain.fullName}</p>
+                        <p className="">{boat.fullName}</p>
                     </div>
                 </div>
                 <div className="grid grid-cols-4 border-t border-stroke px-4 py-4.5 dark:border-strokedark sm:grid-cols-4 md:px-6 2xl:px-7.5">
@@ -79,7 +78,7 @@ export default function BoatDetails() {
                         <p className="font-medium">Captain Phone:</p>
                     </div>
                     <div className="col-span-3 flex items-center">
-                        <p className="">{boat.data.captain.phone}</p>
+                        <p className="">{boat.phone}</p>
                     </div>
                 </div>
                 <div className="grid grid-cols-4 border-t border-stroke px-4 py-4.5 dark:border-strokedark sm:grid-cols-4 md:px-6 2xl:px-7.5">
@@ -87,7 +86,7 @@ export default function BoatDetails() {
                         <p className="font-medium">Captain Email:</p>
                     </div>
                     <div className="col-span-3 flex items-center">
-                        <p className="">{boat.data.captain.email}</p>
+                        <p className="">{boat.email}</p>
                     </div>
                 </div>
                 <div className="grid grid-cols-4 border-t border-stroke px-4 py-4.5 dark:border-strokedark sm:grid-cols-4 md:px-6 2xl:px-7.5">
@@ -95,7 +94,7 @@ export default function BoatDetails() {
                         <p className="font-medium">City:</p>
                     </div>
                     <div className="col-span-3 flex items-center">
-                        <p className="">{boat.data.captain.city}</p>
+                        <p className="">{boat.city}</p>
                     </div>
                 </div>
                 <div className="grid grid-cols-4 border-t border-stroke px-4 py-4.5 dark:border-strokedark sm:grid-cols-4 md:px-6 2xl:px-7.5">
@@ -103,7 +102,7 @@ export default function BoatDetails() {
                         <p className="font-medium">Country:</p>
                     </div>
                     <div className="col-span-3 flex items-center">
-                        <p className="">{boat.data.captain.country}</p>
+                        <p className="">{boat.country}</p>
                     </div>
                 </div>
             </div>
